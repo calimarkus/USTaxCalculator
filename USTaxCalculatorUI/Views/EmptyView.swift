e8dc0fe6bfdccedc1f63d206aca994d956dca6fa
Markus Emrich <Markus.Emrich@gmail.com>
2022-05-08 19:46:06 +0900

    tweak empty view

--- USTaxCalculatorUI/Views/EmptyView.swift
+++ USTaxCalculatorUI/Views/EmptyView.swift
@@ -5,23 +5,25 @@ import SwiftUI
 
 struct EmptyView: View {
     var body: some View {
-        Text("")
-            .padding()
-            .navigationTitle("")
-            .toolbar {
-                ToolbarItem(placement: .status) {
-                    Button {
-                        // TBD
-                    } label: {
-                        Image(systemName: "plus")
-                    }
-                }
-            }
+        VStack(spacing:0.0) {
+            Image(systemName: "pencil.circle.fill")
+                .imageScale(.large)
+                .font(.system(size: 42, weight: .bold))
+            Text("Nothing selected.")
+                .padding()
+                .navigationTitle("")
+            Button {
+                // tbd
+            } label: {
+                Text("Add a dataset")
+            }.padding()
+        }
     }
 }
 
 struct EmptyView_Previews: PreviewProvider {
     static var previews: some View {
         EmptyView()
+            .padding()
     }
 }
